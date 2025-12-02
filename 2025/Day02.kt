6ed89e91275edd85af07639d49d6033644ce43f6
Benedek Árvay <arvaybenedek@gmail.com>
2025-12-02 19:34:06 +0100

    Refactor regex usage and improve readability

--- 2025/Day02.kt
+++ 2025/Day02.kt
@@ -1,16 +1,19 @@
 fun main() {
     val ranges = readInput("input.txt")[0]
         .split(",")
-        .map { it -> it.split("-").map{it.toLong()}  }
+        .map { it.split("-").map { num -> num.toLong() } }
         .map { (start, end) -> start..end }
+
+    val r1 = Regex("""(\d+)\1""")
+    val r2 = Regex("""(\d+)\1+""")
 
     val p1 = ranges
         .flatMap { it }
-        .filter { Regex("""(\d+)\1""").matches(it.toString()) }
+        .filter { r1.matches(it.toString()) }
         .sum()
     val p2 = ranges
         .flatMap { it }
-        .filter { Regex("""(\d+)\1+""").matches(it.toString()) }
+        .filter { r2.matches(it.toString()) }
         .sum()
 
     println("$p1\n$p2")
