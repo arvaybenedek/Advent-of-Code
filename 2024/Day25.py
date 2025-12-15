afab2013bb1ba497e714cd8f4d5f716cd4921be5
arvaybenedek <arvaybenedek@gmail.com>
2025-12-15 10:48:52 +0100

    .

--- 2024/Day25.py
+++
@@ -1,9 +0,0 @@
-from utils import *
-
-with open("input.txt") as f:
-    a = f.read().strip().split('\n\n')
-
-ls = [[sum(row[col] == '#' for row in i.splitlines()) - 1 for col in range(5)] for i in a if i[0] == '#']
-ks = [[sum(row[col] == '#' for row in i.splitlines()) - 1 for col in range(5)] for i in a if i[0] != '#']
-
-print(sum(all(i + j < 6 for i, j in zip(l, k)) for l in ls for k in ks))
