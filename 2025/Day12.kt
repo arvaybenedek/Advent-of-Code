d57a5ade28f0a4fc5773fd8dec131d8cb7125dde
arvaybenedek <arvaybenedek@gmail.com>
2025-12-15 10:46:08 +0100

    Remove solutions for 2025

--- 2025/Day12.kt
+++
@@ -1,7 +0,0 @@
-fun main() = println(readInput("input.txt").count {
-    it.split(": ").takeIf { it.size == 2 }?.let { (d, a) ->
-        d.split("x").map { it.toInt() }.let { (x, y) ->
-            x * y > a.split(" ").map { it.toInt() }.zip(listOf(7, 7, 7, 5, 7, 6)).sumOf { (a, b) -> a * b }
-        }
-    } ?: false
-})
--- 2025/README.md
+++
@@ -1,1 +0,0 @@
-The `Utils.kt` file, which contains helper functions and data structures used across multiple days, will be made available only after the 12th day.
